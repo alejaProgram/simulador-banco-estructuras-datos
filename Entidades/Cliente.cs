3b48725036ccfd03787c77402cb4b78d2cae3f0e
María Alejandra Gonzales Cardenas <111323664+alejaProgram@users.noreply.github.com>
2026-04-28 13:17:28 -0500

    en esta nueva actualización le agregué el constructor a la clase cliente

--- Entidades/Cliente.cs
+++ Entidades/Cliente.cs
@@ -8,5 +8,13 @@ namespace Entidades
         public string NombreCompleto { get; set; }
         public string NumeroCuenta { get; set; }
         public decimal Saldo { get; set; }
+
+        public Cliente(string identificacion, string nombreCompleto, string numeroCuenta, decimal saldoInicial = 0)
+        {
+            Identificacion = identificacion;
+            NombreCompleto = nombreCompleto;
+            NumeroCuenta = numeroCuenta;
+            Saldo = saldoInicial;
+        }
     }
 }
