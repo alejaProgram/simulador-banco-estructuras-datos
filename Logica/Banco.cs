4bd7698ff72de20a292ef213f3161aca4b96e991
María Alejandra Gonzales Cardenas <111323664+alejaProgram@users.noreply.github.com>
2026-05-06 15:47:02 -0500

    Creé clase Banco con esqueleto básico

--- Logica/Banco.cs
+++ Logica/Banco.cs
@@ -10,60 +10,45 @@ namespace Logica
         public Banco()
         {
             clientes = new ListaEnlazadaClientes();
+
         }
+
         public bool RegistrarCliente(string identificacion, string nombreCompleto, string numeroCuenta, decimal saldoInicial = 0)
         {
+
             return false;
         }
 
         public Cliente BuscarCliente(string identificacion)
         {
+
+            return null;
+        }
+
+        public Cliente BuscarClientePorCuenta(string numeroCuenta)
+        {
+
             return null;
         }
 
         public Cliente[] ListarClientes()
         {
+
             return null;
         }
 
         public int ObtenerTotalClientes()
         {
+
             return 0;
         }
 
         public decimal ObtenerTotalDinero()
         {
+
             return 0;
         }
 
-        public bool AgregarACola(string identificacion)
-        {
-            return false;
-        }
 
-        public Cliente AtenderSiguiente()
-        {
-            return null;
-        }
-
-        public bool RealizarDeposito(string numeroCuenta, decimal monto)
-        {
-            return false;
-        }
-
-        public bool RealizarRetiro(string numeroCuenta, decimal monto)
-        {
-            return false;
-        }
-
-        public decimal ConsultarSaldo(string numeroCuenta)
-        {
-            return 0;
-        }
-
-        public bool DeshacerUltimaTransaccion()
-        {
-            return false;
-        }
     }
 }
